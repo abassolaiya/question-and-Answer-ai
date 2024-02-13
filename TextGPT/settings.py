fc0af76e0d1ff16f47ed04b635bf8f719db70c42
Abass Olaiya <ayotundeolaiya@gmail.com>
2024-02-13 06:49:29 +0100

    now work

--- TextGPT/settings.py
+++ TextGPT/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^q_hawke7$9o!iij*i*jr_z2#2l=063u3s&d_$!_0#_(#zscc8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
